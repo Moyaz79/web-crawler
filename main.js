d47c42a9518d12b9e877419adc92b059339345c1
Moyaz79 <yasinabdu77@outlook.com>
2023-08-17 17:42:30 +0100

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('hello World')
+const { crawlPage } = require('./crawl.js')
+
+function main () {
+   if (process.argv.length < 3) {
+       console.log('No enough argument been interd')
+        process.exit(1)
+   }  else if (process.argv.length > 3) {
+        console.log('enterd argument exceed the required one')
+        process.exit(1)
+   }
+   const baseURL = process.argv[2]
+
+   console.log(`Starting crawl of ${baseURL}`)
+   crawlPage(baseURL)
+}
+
+
+main()
